cc9336a8ba83c136c5d83c7a3fc031bc06684908
Bohdan-Serhiyovych <podz1teacher@gmail.com>
2026-02-01 14:43:38 +0200

    my first Class about Student

---
+++ myClass.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, age, height, everageMark):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.everageMark = everageMark
+
+
+
+
+
+mila = Student(name="Mila", age=13, height=167, everageMark=12)
+print(mila.name, mila.age, mila.height, mila.everageMark)
+
+kyrylo = Student(name="Kyrylo", age=12, height=152, everageMark=12)
+print(kyrylo.name, kyrylo.age, kyrylo.height, kyrylo.everageMark)
